390a8a5f98e01beb4df907388bd2de52062ab6a4
Cris Hall-Ramos <crishallramos@yahoo.com>
2025-05-21 11:27:57 -0400

    json

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
-#include "soh/soh/Enhancements/randomizer/3drando/hints.hpp"
-#include "soh/soh/Enhancements/randomizer/randomizerTypes.h"
 #include "soh/soh/Enhancements/randomizer/static_data.h"
 
 #include <iostream>
+#include <nlohmann/json.hpp>
+
 std::array<HintText, RHT_MAX> Rando::StaticData::hintTextTable = {};
+using namespace Rando;
 
 int main(int argc, char **argv) {
   for (int i = 0; i < argc; i++)
     std::cout << "  arv[" << i << "] = " << argv[i] << std::endl;
 
-  Rando::StaticData::HintTable_Init_Exclude_Overworld();
-  Rando::StaticData::HintTable_Init_Exclude_Dungeon();
-  Rando::StaticData::HintTable_Init_Item();
+  StaticData::HintTable_Init_Exclude_Overworld();
+  StaticData::HintTable_Init_Exclude_Dungeon();
+  StaticData::HintTable_Init_Item();
 
-  std::cout << "static data hint len: "
-            << Rando::StaticData::hintTextTable.size() << std::endl;
+  std::cout << "static data hint len: " << StaticData::hintTextTable.size()
+            << std::endl;
+
+  nlohmann::json hint_data;
+  hint_data["hintCount"] = StaticData::hintTextTable.size();
+
+  std::cout << hint_data.dump(2) << std::endl;
 }
